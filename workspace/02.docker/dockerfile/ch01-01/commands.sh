57dca85f7a35014611c151b14157452e6b946563
yurim-study-dev <lyl515222@gmail.com>
2026-08-25 17:40:40 +0900

    docker 자료 및 실습파일 업데이트

---
+++ workspace/02.docker/dockerfile/ch01-01/commands.sh
@@ -0,0 +1,14 @@
+#이미지 빌드
+docker build -t ch01-01-image:latest .
+
+
+# 잘못햇을떄 삭제 docker rm -f ch01-01-container
+
+#컨테이너 구동
+docker run -d --name ch01-01-container -p 80:80 ch01-01-image
+
+
+
+
+#컨테이너 쉘 진입
+docker exec -it ch01-01-container //bin/sh
